67257c0ebee5f5d4beae65791279cab1dd20d7fe
ValentinBatista <valentinbatistaquevedo@gmail.com>
2026-07-09 16:32:13 -0300

    Add files via upload

---
+++ Area 2/TrabajoCoso/Videojuego/Acciones.java
@@ -0,0 +1,5 @@
+package Videojuego;
+
+public class Acciones {
+
+}
---
+++ Area 2/TrabajoCoso/Videojuego/Gestor.java
@@ -0,0 +1,10 @@
+package Videojuego;
+
+public class Gestor {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
