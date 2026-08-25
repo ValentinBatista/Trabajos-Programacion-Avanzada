70fe29122c1bd2304982b0301547440e5b3420b0
ValentinBatista <valentinbatistaquevedo@gmail.com>
2026-08-25 16:37:17 -0300

    Add files via upload

---
+++ Area 3 /Segunda Implementacion - La Calculadora/GestorCalculadora.java
@@ -0,0 +1,12 @@
+package ActividadCalculadora;
+
+import Practico2.Ventana2;
+
+public class GestorCalculadora {
+
+	public static void main(String[] args) {
+		VentanaCalcuadora calculadora1 = new VentanaCalcuadora();
+		calculadora1.setVisible(true);
+	}
+
+}
